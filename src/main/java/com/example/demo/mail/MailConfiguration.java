b240b908b56f203acb33895b69839552ad8de65d
ITSTEP\st <auske_sz16@student.itstep.by>
2024-02-26 19:56:02 +0300

    initial commit

---
+++ src/main/java/com/example/demo/mail/MailConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.demo.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Autowired
+    private Environment env;
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(587);
+
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword("hbsd orgj jcum wzje");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
