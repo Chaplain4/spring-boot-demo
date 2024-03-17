1aa05a6b35da7feee079f80380a68121bdd73935
Arthur Auskern <Chaplain04@gmail.com>
2024-03-17 16:52:15 +0300

    Clients task finished

--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -6,6 +6,9 @@ import java.util.List;
 public interface ClientService {
 
     List<Client> getAllClients();
+    Client getClientById(int id);
     boolean deleteClientById(int id);
-    boolean saveOrUpdateClient(int id);
+    boolean saveOrUpdateClient(Client client);
+
+    boolean createClient(Client client);
 }
--- src/main/java/com/example/demo/service/ClientServiceImpl.java
+++ src/main/java/com/example/demo/service/ClientServiceImpl.java
@@ -4,25 +4,56 @@ import com.example.demo.model.Client;
 import com.example.demo.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class ClientServiceImpl implements ClientService{
+public class ClientServiceImpl implements ClientService {
     @Autowired
     ClientRepository cr;
 
     @Override
     public List<Client> getAllClients() {
-        return null;
+        return cr.findAll();
+    }
+
+    @Override
+    public Client getClientById(int id) {
+        return cr.getReferenceById(id);
     }
 
     @Override
     public boolean deleteClientById(int id) {
-        return false;
+        try {
+            cr.deleteById(id);
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return false;
+        }
     }
 
     @Override
-    public boolean saveOrUpdateClient(int id) {
-        return false;
+    public boolean saveOrUpdateClient(Client client) {
+        try {
+            System.out.println(client.getId());
+            cr.deleteById(client.getId());
+            cr.save(client);
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean createClient(Client client) {
+        try {
+            cr.save(client);
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return false;
+        }
     }
 }
