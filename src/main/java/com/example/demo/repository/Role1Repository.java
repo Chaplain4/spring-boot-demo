6f7e506314fd8f38860df1142c878e721f18f604
ITSTEP\st <auske_sz16@student.itstep.by>
2024-03-25 21:45:09 +0300

    Classwork commited

---
+++ src/main/java/com/example/demo/repository/Role1Repository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Role1;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Role1Repository extends JpaRepository<Role1, Long> {
+    Role1 findByName(String name);
+}
